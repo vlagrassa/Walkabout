4df3563d9eca3039725e5590394ce34ffb7a5c95
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-03 16:27:06 -0400

    Basic main file w test

---
+++ src/Main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include "region/Region.h"
+
+int main() {
+    Skippable test(5);
+    
+    //int x = test.getTestValue();
+    //std::cout << x;
+    //std::cout << test.getTestValue();
+    
+    return EXIT_SUCCESS;
+}

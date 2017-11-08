d2d08e194a426d06ecc57098af09bb8fd9509421
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-07 19:45:56 -0500

    Add basic imports and con/destructors

---
+++ src/region/Encounterable.cpp
@@ -0,0 +1,13 @@
+#include "Encounterable.hpp"
+#include "Region.hpp"
+#include <SFML/Graphics.hpp>
+
+Encounterable::Encounterable() {
+    
+}
+Encounterable::Encounterable(const Encounterable& orig) {
+    
+}
+Encounterable::~Encounterable() {
+    
+}
---
+++ src/region/Room.cpp
@@ -0,0 +1,16 @@
+#include "Room.hpp"
+#include "Region.hpp"
+#include <SFML/Graphics.hpp>
+
+Room::Room(int d, Encounterable t) {
+    
+}
+Room::Room(int d) {
+    
+}
+Room::Room(const Room& orig) {
+    
+}
+Room::~Room() {
+    
+}

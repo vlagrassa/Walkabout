283da0faa4c8788db8bd9d12712d5ed7e57e4987
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-07 19:47:57 -0500

    Add tests for Hall

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,13 +5,34 @@
 #include <cstdlib>
 #include <iostream>
 #include "region/Region.hpp"
+#include "region/Room.hpp"
 
 int main() {
     Skippable test(10);
     
     int x = test.getTestValue();
-    std::cout << x;
-    //std::cout << test.getTestValue();
-    return EXIT_SUCCESS;
+    std::cout << x << "\n";
+    std::cout << test.getTestValue() << "\n";
     
+    Hall h;
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    h.stepRight();
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    h.stepRight(5);
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    h.stepLeft();
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    h.stepLeft(3);
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    h.step(-10);
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    h.step(8);
+    std::cout << "Player X = " << h.getPlayerX() << "\n";
+    
+    h.addRoom(9);
+    std::cout << h.getActiveRoom();
+    
+    //Room r;
+    
+    return EXIT_SUCCESS;
 }

76d369b164867ba4f299b21bc157c315b06b7602
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-12 14:50:40 -0500

    Begin basics of FightScreen class

---
+++ src/screen/FightScreen.hpp
@@ -0,0 +1,14 @@
+#ifndef FIGHTSCREEN_H
+#define FIGHTSCREEN_H
+
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+
+class FightScreen: public ScreenMode {
+public:
+    
+private:
+    
+};
+
+#endif /* FIGHTSCREEN_H */

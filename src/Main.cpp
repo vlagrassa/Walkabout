7b1555b421f8c629a28b58990c1a20d0bdbca718
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-13 15:41:09 -0500

    Add tests for overriding
    Broken.

--- src/Main.cpp
+++ src/Main.cpp
@@ -35,23 +35,42 @@ int main() {
     h.updateIndex(player);
     std::cout << player << "\n" << h << "\n";
     
+    std::cout << "\nRunning Encounterable.getEncounterScreen():\n";
     Encounterable e;
     e.getEncounterScreen();
     
+    std::cout << "\nRunning Monster.getEncounterScreen:\n";
     mon.getEncounterScreen();
     
+    std::cout << "\nStoring Monster.getEncounterScreen() to EncounterScreen* variable:\n";
     EncounterScreen* encounterscreen = mon.getEncounterScreen();
     std::cout << encounterscreen->testThing() << "\n";
     
+    std::cout << "\nStoring Monster.getEncounterScreen() to FightScreen* variable:\n";
     FightScreen* fightscreen = mon.getEncounterScreen();
     std::cout << fightscreen->testThing() << "\n";
     
-    std::vector<EncounterScreen*> screenList;
-    screenList.push_back(mon.getEncounterScreen());
-    screenList.push_back(e.getEncounterScreen());
-    screenList.push_back(mon.getEncounterScreen());
-    screenList.push_back(e.getEncounterScreen());
+    std::cout << "\nCreating Room storing Monster class:\n";
+    Monster tempMon;
     
+    Room testRoom(6, tempMon);
+    
+    std::cout << "\nPrinting encounter from Room:\n";
+    Encounterable* testEncounter = testRoom.getEncounter();
+    std::cout << testEncounter << "\n";
+    testEncounter->encounter();
+    
+    std::cout << "\nPrinting encounter from Room as Monster";
+    Monster* testMonster = testRoom.getEncounter();
+    std::cout << testMonster << "\n";
+    testMonster->encounter();
+    
+    
+    //Room* tempRoom = new Room(6, tempMon);
+    //std::cout << "Testing overriding or whatever\n";
+    //tempRoom->getEncounter()->encounter();
+    
+    /*
     std::cout << "\n\n";
     for (unsigned i = 0; i < h.getListOfRooms().size(); i++) {
     //for (Room* r : h.getListOfRooms()) {
@@ -59,29 +78,51 @@ int main() {
         RoomType temp = r->getType();
         std::cout << temp << " ";
         switch (temp) {
+            
             case (monster): {
                 std::cout << i << ":\n";
                 std::cout << "Getting Encounter:" << "\n";
                 Encounterable* e = r->getEncounter();
                 std::cout << "Default Encounter is " << e << "\n";
-                Monster* tempEncounter = static_cast<Monster*>(e);
+                Monster* tempEncounter = reinterpret_cast<Monster*>(e);
                 //tempEncounter->initialize();
-                std::cout << "Monster* TempEncounter is " << tempEncounter << "\n";
+                std::cout << "\nMonster* TempEncounter is " << tempEncounter << "\n";
                 std::cout << "Getting Screen:" << "\n";
-                FightScreen* tempScreen = tempEncounter->getEncounterScreen();
+                //FightScreen* tempScreen = tempEncounter->getEncounterScreen();
+                //std::cout << tempScreen->testThing() << "\n";
                 //FightScreen* tempScreen = dynamic_cast<FightScreen*>(r->getScreen());
                 //std::cout << tempEncounter->getEncounterScreen()->testThing() << "\n";
                 //tempEncounter->getEncounterScreen();
                 //std::cout << tempEncounter->getName() << "\n";
                 break;
             }
+            
+            case (monster): {
+                std::cout << i << ":\n";
+                std::cout << "Getting Encounter:" << "\n";
+                Encounterable* e = r->getEncounter();
+                std::cout << "Default Encounter is " << e << "\n";
+                
+                std::cout << "Getting Screen:" << "\n";
+                //Monster* reinterpret = reinterpret_cast<Monster*>(e);
+                //Monster* dynamic = dynamic_cast<Monster*>(e);
+                //Monster* staticCast = static_cast<Monster*>(e);
+                
+                //std::cout << reinterpret << "\n";
+                //std::cout << dynamic << "\n";
+                //std::cout << staticCast << "\n";
+                break;
+            }
+            
             default: {
-                std::cout << r->getEncounter()->getEncounterScreen()->testThing() << "\n";
+                //std::cout << r->getEncounter()->getEncounterScreen()->testThing() << "\n";
+                std::cout << "Encounter " << i << " is not viable.\n";
                 break;
             }
         }
         std::cout << "\n";
     }
+    */
     
     return EXIT_SUCCESS;
 }

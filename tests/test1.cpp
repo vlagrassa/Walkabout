cb62ebb5e00699e95e4b8e4c58e5db32afd98292
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-13 16:41:25 -0500

    Create tests folder and first test file

---
+++ tests/test1.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include "../src/region/Room.hpp"
+
+int main() {
+    
+}

3f181469a42f72bc71a8b2087c6391b6ca0818b7
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-13 21:54:23 -0500

    Remove original, obsolete switch statement

--- src/Main.cpp
+++ src/Main.cpp
@@ -54,59 +54,5 @@ int main() {
         }
     }
     
-    /*
-    std::cout << "\n\n";
-    for (unsigned i = 0; i < h.getListOfRooms().size(); i++) {
-    //for (Room* r : h.getListOfRooms()) {
-        Room* r = h.getRoom(i);
-        RoomType temp = r->getType();
-        std::cout << temp << " ";
-        switch (temp) {
-            
-            case (monster): {
-                std::cout << i << ":\n";
-                std::cout << "Getting Encounter:" << "\n";
-                Encounterable* e = r->getEncounter();
-                std::cout << "Default Encounter is " << e << "\n";
-                Monster* tempEncounter = reinterpret_cast<Monster*>(e);
-                //tempEncounter->initialize();
-                std::cout << "\nMonster* TempEncounter is " << tempEncounter << "\n";
-                std::cout << "Getting Screen:" << "\n";
-                //FightScreen* tempScreen = tempEncounter->getEncounterScreen();
-                //std::cout << tempScreen->testThing() << "\n";
-                //FightScreen* tempScreen = dynamic_cast<FightScreen*>(r->getScreen());
-                //std::cout << tempEncounter->getEncounterScreen()->testThing() << "\n";
-                //tempEncounter->getEncounterScreen();
-                //std::cout << tempEncounter->getName() << "\n";
-                break;
-            }
-            
-            case (monster): {
-                std::cout << i << ":\n";
-                std::cout << "Getting Encounter:" << "\n";
-                Encounterable* e = r->getEncounter();
-                std::cout << "Default Encounter is " << e << "\n";
-                
-                std::cout << "Getting Screen:" << "\n";
-                //Monster* reinterpret = reinterpret_cast<Monster*>(e);
-                //Monster* dynamic = dynamic_cast<Monster*>(e);
-                //Monster* staticCast = static_cast<Monster*>(e);
-                
-                //std::cout << reinterpret << "\n";
-                //std::cout << dynamic << "\n";
-                //std::cout << staticCast << "\n";
-                break;
-            }
-            
-            default: {
-                //std::cout << r->getEncounter()->getEncounterScreen()->testThing() << "\n";
-                std::cout << "Encounter " << i << " is not viable.\n";
-                break;
-            }
-        }
-        std::cout << "\n";
-    }
-    */
-    
     return EXIT_SUCCESS;
 }

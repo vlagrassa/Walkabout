1ea10644ee35094ad11f50229d19c45dc83eb566
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-14 15:25:50 -0500

    Clean up Encounterable class

--- src/region/Encounterable.cpp
+++ src/region/Encounterable.cpp
@@ -3,8 +3,10 @@
 #include <cstdlib>
 #include <iostream>
 
+// <editor-fold defaultstate="collapsed" desc=" Con/Destructors ">
+
 Encounterable::Encounterable() {
-    initScreen();
+    screen = new EncounterScreen();
 }
 Encounterable::Encounterable(const Encounterable& orig) {
     
@@ -13,15 +15,19 @@ Encounterable::~Encounterable() {
     
 }
 
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc=" Inherited Methods ">
+
+/* Returns a list of Items held by the Encounterable */
+std::vector<int> Encounterable::getReward() const {
+    return reward;
+}
+
+/* Returns the EncounterScreen associated with the Encounterable */
 EncounterScreen* Encounterable::getEncounterScreen() const {
     std::cout << "Returning default encounter screen!" << "\n";
     return screen;
 }
 
-std::vector<int> Encounterable::getReward() const {
-    return reward;
-}
-
-void Encounterable::initScreen() {
-    screen = new EncounterScreen();
-}
+// </editor-fold>
--- src/region/Encounterable.hpp
+++ src/region/Encounterable.hpp
@@ -6,7 +6,6 @@
 #include "../screen/EncounterScreen.hpp"
 
 enum RoomType {
-    unknown,
     monster,
     treasure,
     puzzle
@@ -18,23 +17,33 @@ public:
     Encounterable(const Encounterable& orig);
     virtual ~Encounterable();
     
-    virtual bool isSkippable() = 0;
+    /* Inherited */
     
-    virtual std::string getTypeName() const = 0;
-    
-    virtual EncounterScreen* getEncounterScreen() const;
-    
-    void initScreen();
-    
+    /* Returns a list of Items held by the Encounterable - as far as I can tell, all Encounterable subclasses will have one */
     virtual std::vector<int> getReward() const; //should return Item or something
     
-    virtual void encounter() = 0;
+    /* Returns the EncounterScreen associated with the Encounterable */
+    virtual EncounterScreen* getEncounterScreen() const;
     
+    /* Abstract Methods */
+    
+    /* Returns whether the Encounter can be passed without interacting with it - this value is the same for each subclass */
+    virtual bool isSkippable() = 0;
+    
+    /* Returns a string associated with the given subclass, eg "Monster" */
+    virtual std::string getTypeName() const = 0;
+    
+    /* Returns the RoomType enum value associated with the given subclass */
     virtual RoomType getType() const = 0;
     
+    /* Interact with the Encounterable - will vary for each subclass */
+    virtual void encounter() = 0;
+    
 private:
-    //bool skippable;
+    /* A pointer to the associated EncounterScreen */
     EncounterScreen* screen;
+    
+    /* The list of Items associated with the given Encounterable */
     std::vector<int> reward;
 };
 

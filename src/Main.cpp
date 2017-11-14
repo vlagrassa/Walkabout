af1b47ed9e3ac9d2fc077390181e1ff6d9553dbe
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-11-13 21:18:27 -0500

    Remove old tests still in Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,69 +12,6 @@
 
 int main() {
     
-    std::cout << "\n\n";
-    
-    Monster mon;
-    
-    Hall h(5);
-    h.addRoom();
-    h.addRoom();
-    h.addRoom(new Room(6, &mon));
-    h.getRoom(2)->setType(monster);
-    
-    Player player;
-    //Menu pauseScreen;
-    
-    std::cout << player << "\n";
-    
-    player.goTo(4);
-    h.updateIndex(player);
-    std::cout << player << "\n" << h << "\n";;
-    
-    player.stepRight();
-    h.updateIndex(player);
-    std::cout << player << "\n" << h << "\n";
-    
-    std::cout << "\nRunning Encounterable.getEncounterScreen():\n";
-    Encounterable e;
-    e.getEncounterScreen();
-    
-    std::cout << "\nRunning Monster.getEncounterScreen:\n";
-    mon.getEncounterScreen();
-    
-    std::cout << "\nStoring Monster.getEncounterScreen() to EncounterScreen* variable:\n";
-    EncounterScreen* encounterscreen = mon.getEncounterScreen();
-    std::cout << encounterscreen->testThing() << "\n";
-    
-    std::cout << "\nStoring Monster.getEncounterScreen() to FightScreen* variable:\n";
-    FightScreen* fightscreen = mon.getEncounterScreen();
-    std::cout << fightscreen->testThing() << "\n";
-    
-    std::cout << "\nCreating Room storing Monster class:\n";
-    Monster tempMon;
-    
-    Room testRoom(6, &tempMon);
-    
-    std::cout << "\nPrinting encounter from Room:\n";
-    Encounterable* testEncounter = testRoom.getEncounter();
-    std::cout << testEncounter << "\n";
-    testEncounter->encounter();
-    
-    std::cout << "\nPrinting encounter from Room as Monster\n";
-    Monster* testMonster = (Monster*)(testRoom.getEncounter());
-    std::cout << testMonster << "\n";
-    testMonster->getName();
-    
-    std::cout << "\nStoring new Monster* as Encounterable*:\n";
-    Encounterable* testThing = new Monster();
-    testThing->encounter();
-    
-    std::cout << "\nStoring polymorphed Monster in a Room\n";
-    Room* monsterRoom = new Room(5, testThing);
-    Monster* testMonster2 = (Monster*) monsterRoom->getEncounter();
-    std::cout << "Pointer to monster: " <<  testMonster2 << "\n";
-    testMonster2->encounter();
-    
     //Room* tempRoom = new Room(6, tempMon);
     //std::cout << "Testing overriding or whatever\n";
     //tempRoom->getEncounter()->encounter();

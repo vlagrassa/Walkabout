2688ecf31935904fa85607352734118885a89cf1
Alisa Khuu <alisakhuu@gmail.com>
2017-11-16 10:35:12 -0500

    Add Window

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,8 +9,11 @@
 #include "player/Player.hpp"
 #include "screen/Menu.hpp"
 #include "screen/FightScreen.hpp"
+#include <SFML/Window.hpp>
 
 int main() {
+    
+    sf::Window window(sf::VideoMode(800, 600), "Walkabout");
     
     std::cout << "\n\n=-=-= This is the start of Main =-=-=\n\n";
     
@@ -32,6 +35,19 @@ int main() {
     for (Room* r : h) {
         std::cout << *r << "\n";
     }
+    
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     
     return EXIT_SUCCESS;
 }

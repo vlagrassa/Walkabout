aa3e1b8b22919d8b476638cbddfc7eddcc3174c7
Alisa Khuu <alisakhuu@gmail.com>
2017-11-26 15:46:10 -0500

    Created Ambience class
    holds textures for specific backgrounds

---
+++ src/window/Ambience.hpp
@@ -0,0 +1,16 @@
+#ifndef AMBIENCE_H
+#define AMBIENCE_H
+
+#include <SFML/Graphics.hpp>
+#include <stdlib.h> 
+
+class Ambience {
+public:
+    Ambience();
+    Ambience(const Ambience& orig);
+    virtual ~Ambience();
+private:
+    
+};
+
+#endif /* AMBIENCE_H */

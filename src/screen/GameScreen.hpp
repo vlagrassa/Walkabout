4a8801870c21df308ac9b199c69655e52d51474b
FordPrefect42 <Yaakovred@gmail.com>
2017-12-01 10:22:46 -0500

    Made some functions for GameScreen
    ...

---
+++ src/screen/GameScreen.hpp
@@ -0,0 +1,25 @@
+#ifndef GAMESCREEN_H
+#define GAMESCREEN_H
+
+#include "ScreenMode.hpp"
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include "../player/Player.hpp"
+#include "../region/Hall.hpp"
+#include "../window/Background.hpp"
+
+class GameScreen: public ScreenMode {
+public:
+    GameScreen();
+    GameScreen(const GameScreen&);
+    virtual ~GameScreen();
+    
+    virtual std::string testThing();
+    void drawStuff();
+private:
+    Player* player;
+    Hall* hall;
+    Background* background;
+};
+
+#endif /* GAMESCREEN_H */

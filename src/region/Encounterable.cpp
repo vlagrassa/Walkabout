8264295713e3b7fc95ab53afd98ab79f0bed0feb
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-12-06 11:25:52 -0500

    Copy comments from header file

--- src/region/Encounterable.cpp
+++ src/region/Encounterable.cpp
@@ -5,13 +5,28 @@
 
 // <editor-fold defaultstate="collapsed" desc=" Con/Destructors ">
 
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 
+ * Default constructor method - can't be used anyways, this
+ * is an abstract class.
+ */
 Encounterable::Encounterable() {
     screen = new EncounterScreen();
-    
 }
+
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 
+ * Default constructor method.
+ */
 Encounterable::Encounterable(const Encounterable& orig) {
     
 }
+
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 
+ * Default destructor method. Deletes:
+ *   -EncounterScreen screen
+ */
 Encounterable::~Encounterable() {
     delete screen;
 }
@@ -20,12 +35,22 @@ Encounterable::~Encounterable() {
 
 // <editor-fold defaultstate="collapsed" desc=" Inherited Methods ">
 
-/* Returns a list of Items held by the Encounterable */
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 
+ * Get the list of Items held by the Encounterable - as far
+ * as I can tell, all Encounterable subclasses will have one
+ * @return vector of Items held by the Encounterable
+ */
 std::vector<int> Encounterable::getReward() const {
     return reward;
 }
 
-/* Returns the EncounterScreen associated with the Encounterable */
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 
+ * Get the EncounterScreen associated with the Encounterable.
+ * Will be overwritten by each subclass.
+ * @return EncounterScreen representing the encounter
+ */
 EncounterScreen* Encounterable::getEncounterScreen() const {
     std::cout << "Returning default encounter screen!" << "\n";
     return screen;

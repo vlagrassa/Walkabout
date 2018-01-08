d1776e38eb3d9dddacb60b65bd8626262409c6f4
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-08 10:07:26 -0500

    Add LinkedButton class from LatinMap2018 project

--- src/screen/ScreenMode.hpp
+++ src/screen/ScreenMode.hpp
@@ -4,6 +4,48 @@
 #include <SFML/Graphics.hpp>
 #include <stdlib.h>
 
+#include "../utils/Utils.hpp"
+
+class LinkedButton;
+class ScreenMode;
+
+class LinkedButton : public sf::RectangleShape {
+public:
+    ScreenMode& link;
+    sf::Window& window;
+    
+    LinkedButton(ScreenMode& link, sf::Window& window) : link(link), window(window) {
+        setSize(sf::Vector2f(100, 50));
+        setOutlineColor(sf::Color::Red);
+        setOutlineThickness(5);
+        setPosition(10, 20);
+    };
+    
+    LinkedButton(ScreenMode* link, sf::Window& window) : link(*link), window(window) {
+        setSize(sf::Vector2f(100, 50));
+        setOutlineColor(sf::Color::Cyan);
+        setOutlineThickness(5);
+        setPosition(350, 20);
+    }
+    
+    LinkedButton(const LinkedButton& orig) : link(orig.link), window(orig.window) {
+        setSize(sf::Vector2f(100, 50));
+        setOutlineColor(sf::Color::Green);
+        setOutlineThickness(5);
+        setPosition(10, 20);
+    };
+    
+    virtual ~LinkedButton() {};
+    
+    bool touchingMouse() {
+        return getGlobalBounds().contains(sf::Mouse().getPosition(window).x, sf::Mouse().getPosition(window).y);
+    }
+    
+    bool clicked() {
+        return touchingMouse() && sf::Mouse().isButtonPressed(sf::Mouse().Left);
+    }
+};
+
 class ScreenMode {
 public:
     ScreenMode();

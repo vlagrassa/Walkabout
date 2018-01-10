b05abcd9baecc4947a04d4c016bc5ece0f90d6db
CJAdams09 <Yaakovred@gmail.com>
2018-01-10 09:56:22 -0500

    added item class

---
+++ src/player/Items.hpp
@@ -0,0 +1,16 @@
+#ifndef ITEMS_H
+#define ITEMS_H
+
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <iostream>
+
+Items::Items() {
+    
+}
+Items::Items(const Items& orig) {
+    
+}
+Items::~Items() {
+    
+}

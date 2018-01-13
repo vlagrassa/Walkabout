49100cab660e0e358ceb71f423c6f6bcf9673239
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-13 15:54:41 -0500

    Change temp in addMenuOption to pointer & dereference it
    This has solved the original seg fault but causes a new problem when the button is pressed.

--- src/screen/Menu.cpp
+++ src/screen/Menu.cpp
@@ -17,13 +17,13 @@ Menu::~Menu() {
 
 LinkedButton& Menu::addMenuOption(std::string text, sf::Font& font, ScreenMode& link) {
     std::cout << "Parameters of addMenuOption (not LinkedButton):\n\"" << text << "\" linked to " << &link << " in window " << &window << "\n";
-    LinkedButton temp(link, window);
-    temp.setTitle(*new sf::Text(text, font));
-    std::cout << "Temp LinkedButton created in addMenuOption:\n" << temp;
-    LinkedButton result = addButton(temp);
+    LinkedButton* temp = new LinkedButton(link, window);
+    temp->setTitle(*new sf::Text(text, font));
+    std::cout << "Temp LinkedButton created in addMenuOption:\n" << *temp;
+    LinkedButton result = addButton(*temp);
     std::cout << "Returned to addMenuOption from addButton:\n" << result;
     std::cout << "Above printed manually:\n\"" << std::string(result.title.getString()) << "\" linked to " << &result.link << " in window " << &result.window << "\n";
-    return temp;
+    return *temp;
 }
 
 int Menu::get1() const {

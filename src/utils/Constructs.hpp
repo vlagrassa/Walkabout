fbcaf1bc35c0fe4f128564ab7a54969c23c2ba6c
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-19 10:19:04 -0500

    Comment methods & variables in FrameRate class

--- src/utils/Constructs.hpp
+++ src/utils/Constructs.hpp
@@ -38,11 +38,29 @@ public:
     }
 };
 
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ * 
+ * Abstract class with functionality to run the run() method
+ * periodically based on a given frame rate.
+ */
 class FrameRate {
 public:
+    /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     * 
+     * The number of frames that should elapse between each run.
+     */
     unsigned int frameRate;
+    
+    /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     * 
+     * The previous time the object was updated.
+     */
     time_t prevUpdateTime;
     
+    /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     * 
+     * Run in the main loop to update periodically.
+     */
     void updateFrames(time_t newUpdateTime) {
         if (prevUpdateTime + frameRate <= newUpdateTime) {
             prevUpdateTime = newUpdateTime;
@@ -50,6 +68,12 @@ public:
         }
     };
     
+    /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     * 
+     * Define this in each subclass and it will run periodically
+     * based on the given frame rate as long as updateFrames is
+     * called.
+     */
     virtual void run() = 0;
 };
 

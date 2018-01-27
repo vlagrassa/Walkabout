646b64ed380e484c67a7ab406ff63e47a89025ec
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-27 16:39:36 -0500

    Move Oscillator and Slider from Constructs to FightScreen

--- src/screen/FightScreen.cpp
+++ src/screen/FightScreen.cpp
@@ -2,6 +2,64 @@
 #include "../utils/Defaults.hpp"
 #include <SFML/Graphics.hpp>
 
+
+void Oscillator::run(sf::Event event) {
+    attackSlider.currentPos += attackSlider.dir;
+    if (attackSlider.currentPos >= outline.getPosition().x + outline.getSize().x - attackSlider.getSize().x) {
+        attackSlider.dir = std::abs(attackSlider.dir) * -1;
+        attackSlider.currentPos = outline.getPosition().x + outline.getSize().x - attackSlider.getSize().x;
+    } else if (attackSlider.currentPos <= outline.getPosition().x) {
+        attackSlider.dir = std::abs(attackSlider.dir);
+        attackSlider.currentPos = outline.getPosition().x;
+    }
+    attackSlider.setPosition(sf::Vector2f(attackSlider.currentPos, attackSlider.getPosition().y));
+}
+
+void Oscillator::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(areas, 16, sf::TriangleStrip);
+    target.draw(outline);
+    target.draw(attackSlider);
+}
+
+float Oscillator::getStrength() {
+    return 1.0;
+}
+
+void Oscillator::scramble() {
+    srand(DEFAULT_GAMECLOCK.getElapsedTime().asMilliseconds());
+    bool attackOnLeft = rand()%2;
+    sf::Color colors[] = {
+        sf::Color::Black,
+        sf::Color::Black,
+        (attackOnLeft) ? sf::Color::Green : sf::Color::Red,
+        sf::Color::Black,
+        sf::Color::Black,
+        (attackOnLeft) ? sf::Color::Red : sf::Color::Green,
+        sf::Color::Black,
+        sf::Color::Black,
+    };
+    for (unsigned int i = 0; i < 16; i+=2) {
+        areas[i]   = sf::Vertex(sf::Vector2f(outline.getPosition().x + outline.getSize().x/7*i/2, outline.getPosition().y                      ), colors[i/2]);
+        areas[i+1] = sf::Vertex(sf::Vector2f(outline.getPosition().x + outline.getSize().x/7*i/2, outline.getPosition().y + outline.getSize().y), colors[i/2]);
+    }
+}
+
+void Oscillator::initShapes(sf::Vector2f pos) {
+    attackSlider.setFillColor(sf::Color::Cyan);
+    attackSlider.setOutlineColor(sf::Color::Black);
+    attackSlider.setOutlineThickness(3);
+    attackSlider.setPosition(pos);
+    outline.setFillColor(sf::Color::Transparent);
+    outline.setOutlineColor(sf::Color::Black);
+    outline.setOutlineThickness(3);
+    outline.setPosition(pos);
+}
+
+void Oscillator::updateArea() {
+    
+}
+
+
 FightScreen::FightScreen(sf::Window& window) : EncounterScreen(window), attackBar(sf::Vector2f(10,436), sf::Vector2f(780,154), 8) {
     
 }

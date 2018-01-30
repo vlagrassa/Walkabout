97b14634be2f8fa8faea8c03bc22d405a701283b
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-30 16:02:00 -0500

    Remove obsolete methods, group methods for aesthetics

--- src/screen/ScreenMode.hpp
+++ src/screen/ScreenMode.hpp
@@ -23,33 +23,24 @@ public:
     sf::Keyboard::Key key = sf::Keyboard::Unknown;
     
     LinkedButton(ScreenMode& link, sf::RectangleShape rect = DEFAULT_RECT, sf::Window& window = DEFAULT_WINDOW);
-    
     LinkedButton(ScreenMode* link, sf::RectangleShape rect = DEFAULT_RECT, sf::Window& window = DEFAULT_WINDOW);
-    
     LinkedButton(const LinkedButton& orig);
-    
     virtual ~LinkedButton();
     
     void setKey(sf::Keyboard::Key k);
-    
     void setTitle(std::string text);
-    
     void setTitles(std::string text1, std::string text2);
     
     sf::Vector2f getCenter();
     
     bool touchingMouse();
-    
     bool clicked();
     
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     
     bool isActive();
-    
     void setActive(bool b);
-    
     void activate();
-    
     void deactivate();
     
 private:
@@ -67,27 +58,17 @@ public:
     bool showPrevious = false;
     
     ScreenMode(sf::Window& window = DEFAULT_WINDOW);
-    
     ScreenMode(const ScreenMode& orig);
-    
     virtual ~ScreenMode();
     
     virtual ScreenMode* run(sf::Event event);
-    
     ScreenMode* checkButtons(sf::Event event);
     
     void addButton(LinkedButton& b);
-    
     virtual void addButton(std::string title, ScreenMode& link, sf::Keyboard::Key key = sf::Keyboard::Unknown, sf::Font& font = DEFAULT_FONT);
-    
     virtual void addButton(std::string title, ScreenMode* link, sf::Keyboard::Key key = sf::Keyboard::Unknown, sf::Font& font = DEFAULT_FONT);
     
     void addText(sf::Text);
-    
-    bool isActive();
-    void setActive();
-    void activate();
-    void deactivate();
     
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     
@@ -101,15 +82,11 @@ public:
     bool forward = true;
     
     TransitionScreen(ScreenMode* link, sf::Window& window = DEFAULT_WINDOW);
-    
     TransitionScreen(const TransitionScreen& orig);
-    
     virtual ~TransitionScreen();
     
     virtual ScreenMode* run(sf::Event event);
-    
     virtual ScreenMode* runForward();
-    
     virtual ScreenMode* runBackward();
 };
 
